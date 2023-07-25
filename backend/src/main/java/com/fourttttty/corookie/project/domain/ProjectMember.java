e80fe16b1358efe74c4d92941d654d06e3f9a500
rt3310 <btac3310@gmail.com>
2023-07-25 21:02:04 +0900

    feature: ProjectMember 복합키 설정

--- backend/src/main/java/com/fourttttty/corookie/project/domain/ProjectMember.java
+++ backend/src/main/java/com/fourttttty/corookie/project/domain/ProjectMember.java
@@ -2,28 +2,16 @@ package com.fourttttty.corookie.project.domain;
 
 import com.fourttttty.corookie.member.domain.Member;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "project_member")
 @NoArgsConstructor
 public class ProjectMember {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private ProjectMemberId id;
 
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    @ManyToOne
-    @JoinColumn(name = "project_id")
-    private Project project;
-
-    //To-Do : 프로젝트에 Member 추가 / 제거 구현
-    public ProjectMember(Member member, Project project){
-       this.member = member;
-       this.project = project;
+    public ProjectMember(Project project, Member member) {
+        this.id = new ProjectMemberId(project, member);
     }
 }
