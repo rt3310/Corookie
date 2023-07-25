1e843e190cd08cbbd95e870599ea00e583544975
rt3310 <btac3310@gmail.com>
2023-07-25 21:04:28 +0900

    fix: ProjectMember 기본 생성자 접근 제한자 변경 public -> protected

--- backend/src/main/java/com/fourttttty/corookie/project/domain/ProjectMember.java
+++ backend/src/main/java/com/fourttttty/corookie/project/domain/ProjectMember.java
@@ -2,11 +2,14 @@ package com.fourttttty.corookie.project.domain;
 
 import com.fourttttty.corookie.member.domain.Member;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "project_member")
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProjectMember {
     @EmbeddedId
     private ProjectMemberId id;
