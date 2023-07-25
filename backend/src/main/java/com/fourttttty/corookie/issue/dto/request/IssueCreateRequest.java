dc23109b657a2a996ac57489ff087b8ab153a183
rt3310 <btac3310@gmail.com>
2023-07-25 22:27:52 +0900

    fix: IssueCreateRequest validated 적용

--- backend/src/main/java/com/fourttttty/corookie/issue/dto/request/IssueCreateRequest.java
+++ backend/src/main/java/com/fourttttty/corookie/issue/dto/request/IssueCreateRequest.java
@@ -5,15 +5,17 @@ import com.fourttttty.corookie.issue.domain.IssuePriority;
 import com.fourttttty.corookie.issue.domain.IssueProgress;
 import com.fourttttty.corookie.member.domain.Member;
 import com.fourttttty.corookie.project.domain.Project;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
-public record IssueCreateRequest(String topic,
-                                 String description,
-                                 IssueProgress issueProgress,
-                                 IssuePriority issuePriority,
+public record IssueCreateRequest(@NotBlank String topic,
+                                 @NotNull String description,
+                                 @NotNull IssueProgress issueProgress,
+                                 @NotNull IssuePriority issuePriority,
                                  List<IssueCategoryCreateRequest> issueCategories) {
 
     public Issue toEntity(Project project, Member member) {
