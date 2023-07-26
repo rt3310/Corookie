56e016ff25605df92282b08770d7b749f313b5b1
tmd0707 <tmd0707@naver.com>
2023-07-26 12:27:34 +0900

    style : 컨벤션에 따른 수정

--- backend/src/main/java/com/fourttttty/corookie/plan/application/repository/PlanRepository.java
+++ backend/src/main/java/com/fourttttty/corookie/plan/application/repository/PlanRepository.java
@@ -4,7 +4,8 @@ import com.fourttttty.corookie.plan.domain.Plan;
 
 import java.util.Optional;
 
-public interface PlanRepository{
+public interface PlanRepository {
+
     Optional<Plan> findById(Long id);
 
     Plan save(Plan plan);
