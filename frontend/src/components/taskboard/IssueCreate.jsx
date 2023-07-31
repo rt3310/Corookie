7b20cdc57f83ae6c3ef56d2558023771f55270e2
Hyunsoo <imapples@naver.com>
2023-07-31 15:29:20 +0900

    feature: 이슈 리스트 화면 구현

--- frontend/src/components/taskboard/IssueCreate.jsx
+++ frontend/src/components/taskboard/IssueCreate.jsx
@@ -3,20 +3,23 @@ import styled from 'styled-components'
 
 import * as components from 'components'
 import * as hooks from 'hooks'
+import * as utils from 'utils'
 const IssueCreate = () => {
     const { closeIssueCreate } = hooks.issueCreateState()
     const { value: priority, setValue: setPriority } = hooks.createPriorityState()
     const { value: manager, setValue: setManager } = hooks.createManagerState()
     const { value: category, setValue: setCategory } = hooks.createCategoryState()
+    const { closeIssueDetail } = hooks.issueDetailState()
     const [title, setTitle] = useState('')
     let createRef = useRef(null)
-
-    useEffect(() => {}, [])
+    let titleInput = useRef(null)
 
     useEffect(() => {
         setPriority('중요도')
         setManager('관리자')
         setCategory('분류')
+        titleInput.current.focus()
+        closeIssueDetail()
         const handleOutside = e => {
             if (createRef.current && !createRef.current.contains(e.target)) {
                 closeIssueCreate()
@@ -43,13 +46,21 @@ const IssueCreate = () => {
     const categoryList = ['frontend', 'backend', 'design', 'development', 'product', 'other']
     return (
         <S.Container ref={createRef}>
-            <S.Title value={title} onChange={handleTitleChange} placeholder="제목을 입력하세요" />
-            <S.Properties>
-                <components.ToggleButton defaultVal="createManager" list={managerList} />
-                <components.ToggleButton defaultVal="createPriority" list={priorityList} />
-                <components.ToggleButton defaultVal="createCategory" list={categoryList} />
-            </S.Properties>
-            <S.Save onClick={() => saveIssue()}>저장</S.Save>
+            <S.Title ref={titleInput} value={title} onChange={handleTitleChange} placeholder="제목을 입력하세요" />
+            <S.Values>
+                <S.Properties>
+                    <S.ButtonContainer>
+                        <components.ToggleButton defaultVal={utils.ISSUE_OPTIONS.createManager} list={managerList} />
+                    </S.ButtonContainer>
+                    <S.ButtonContainer>
+                        <components.ToggleButton defaultVal={utils.ISSUE_OPTIONS.createPriority} list={priorityList} />
+                    </S.ButtonContainer>
+                    <S.ButtonContainer>
+                        <components.ToggleButton defaultVal={utils.ISSUE_OPTIONS.createCategory} list={categoryList} />
+                    </S.ButtonContainer>
+                </S.Properties>
+                <S.Save onClick={() => saveIssue()}>저장</S.Save>
+            </S.Values>
         </S.Container>
     )
 }
@@ -62,21 +73,32 @@ const S = {
         min-height: 48px;
         margin: 4px 0;
         padding: 8px 16px;
-        border: solid 1px #dbdbdb;
+        border: solid 2px ${({ theme }) => theme.color.main};
         border-radius: 8px;
+        background-color: ${({ theme }) => theme.color.white};
+    `,
+    Values: styled.div`
+        display: flex;
+        justify-content: space-between;
     `,
     Title: styled.input`
+        margin: 0 8px 0 0;
         width: 100%;
         border: none;
         outline: none;
+        font-family: 'Noto Sans KR', 'Pretendard', sans-serif;
+        font-size: ${({ theme }) => theme.fontsize.content};
     `,
     Properties: styled.div`
         display: flex;
         justify-content: space-between;
-        min-width: 310px;
-        & div {
-            padding: 0px;
-        }
+        min-width: 340px;
+    `,
+    ButtonContainer: styled.div`
+        display: flex;
+        justify-content: center;
+        width: 116px;
+        padding: 0 8px;
     `,
     Save: styled.div`
         display: flex;
@@ -84,13 +106,21 @@ const S = {
         align-items: center;
         width: auto;
         padding: 0 8px;
-        margin: 0 8px;
-        border-radius: 4px;
-        white-space: nowrap;
-        background-color: ${({ theme }) => theme.color.main};
         font-size: ${({ theme }) => theme.fontsize.sub1};
+        white-space: nowrap;
+        border-radius: 8px;
+        border: 1px solid ${({ theme }) => theme.color.main};
+        background-color: ${({ theme }) => theme.color.main};
         color: ${({ theme }) => theme.color.white};
+        margin: 0 0 0 8px;
+        transition-duration: 0.2s;
         cursor: pointer;
+
+        &:hover {
+            background-color: ${({ theme }) => theme.color.white};
+            color: ${({ theme }) => theme.color.main};
+            box-shadow: none;
+        }
     `,
 }
 
