769897fa60470d2a9f0d545103beb80c5aa22fab
서원호 <btac3310@gmail.com>
2023-08-02 12:51:37 +0900

    style: 코드 컨벤션 수정

--- backend/src/main/java/com/fourttttty/corookie/global/exception/ProjectNotEnabledException.java
+++ backend/src/main/java/com/fourttttty/corookie/global/exception/ProjectNotEnabledException.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 public class ProjectNotEnabledException extends RuntimeException {
     private final ExceptionCode exceptionCode;
 
-    public ProjectNotEnabledException(){
+    public ProjectNotEnabledException() {
         this(ExceptionCode.PROJECT_NOT_ENABLED);
     }
 
-    public ProjectNotEnabledException(ExceptionCode exceptionCode){
+    public ProjectNotEnabledException(ExceptionCode exceptionCode) {
         super(exceptionCode.getMessage());
         this.exceptionCode = exceptionCode;
     }
