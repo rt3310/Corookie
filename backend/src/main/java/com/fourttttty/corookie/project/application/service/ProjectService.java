b99d6caa310d423bc8ea232edcd8829b2bc39b91
rt3310 <btac3310@gmail.com>
2023-08-10 07:23:29 +0900

    feature: 자신이 속한 프로젝트 조회 기능 구현

--- backend/src/main/java/com/fourttttty/corookie/project/application/service/ProjectService.java
+++ backend/src/main/java/com/fourttttty/corookie/project/application/service/ProjectService.java
@@ -1,9 +1,11 @@
 package com.fourttttty.corookie.project.application.service;
 
 import com.fourttttty.corookie.member.application.repository.MemberRepository;
+import com.fourttttty.corookie.member.domain.Member;
 import com.fourttttty.corookie.project.application.repository.ProjectMemberRepository;
 import com.fourttttty.corookie.project.application.repository.ProjectRepository;
 import com.fourttttty.corookie.project.domain.Project;
+import com.fourttttty.corookie.project.domain.ProjectMember;
 import com.fourttttty.corookie.project.dto.request.ProjectCreateRequest;
 import com.fourttttty.corookie.project.dto.request.ProjectUpdateRequest;
 import com.fourttttty.corookie.project.dto.response.ProjectDetailResponse;
@@ -32,6 +34,12 @@ public class ProjectService {
                 .toList();
     }
 
+    public List<ProjectListResponse> findByParticipantId(Long participantId) {
+        return projectMemberRepository.findByMemberId(participantId).stream()
+                .map(projectMember -> ProjectListResponse.from(projectMember.getId().getProject()))
+                .toList();
+    }
+
     public ProjectDetailResponse findById(Long projectId, Long managerId) {
         Project project = findEntityById(projectId);
         return ProjectDetailResponse.from(project, project.isManager(managerId));
@@ -43,12 +51,17 @@ public class ProjectService {
 
     @Transactional
     public ProjectDetailResponse create(ProjectCreateRequest projectCreateRequest, Long managerId) {
-        Project project = projectRepository.save(projectCreateRequest.toEntity(
-                memberRepository.findById(managerId).orElseThrow(EntityNotFoundException::new)));
+        Member member = memberRepository.findById(managerId).orElseThrow(EntityNotFoundException::new);
+        Project project = projectRepository.save(projectCreateRequest.toEntity(member));
 
+        registerMemberForProject(member, project);
         project.changeInvitationLink(invitationLinkGenerateService.generateInvitationLink((long) project.hashCode()));
         project.createDefaultTextChannels().forEach(textChannelRepository::save);
         return ProjectDetailResponse.from(project, project.isManager(managerId));
+    }
+
+    private void registerMemberForProject(Member member, Project project) {
+        projectMemberRepository.save(ProjectMember.of(project, member));
     }
 
     @Transactional
